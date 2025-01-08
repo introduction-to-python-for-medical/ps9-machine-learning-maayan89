f3d85d8daca2af98433be142981b90f776acbd57
maayan89 <maayan089@gmail.com>
2025-01-08 15:40:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ y = df[target]
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-xscale = scaler.fit_transform(x)
+x_scale = scaler.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(xscale, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x_scale, y, test_size=0.2, random_state=42)
 
 from sklearn.svm import SVC
 model = SVC()
 model.fit(x_train, y_train)
 
 from sklearn.metrics import accuracy_score
-ypred = model.predict(x_test)
-accuracy = accuracy_score(y_test, ypred)
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
